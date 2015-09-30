240d3daf75fe04586830e6383d6d35a6729077fc
cammero <cameolynn@msn.com>
2015-09-30 14:46:23 -0500

    Added opening comment

--- src/com/cameo/DinosaurSupervisor.java
+++ src/com/cameo/DinosaurSupervisor.java
@@ -3,7 +3,7 @@ package com.cameo;
 public class DinosaurSupervisor {
 
     public static void main(String[] args) {
-
+    //Create dinosaur objects
     Dinosaur trex = new Dinosaur();
     Dinosaur velociraptor = new Dinosaur();
     Dinosaur diplodocus = new Dinosaur();
