94f75dbfa1cff735a9cec2c07cb0c9343efc50ef
cammero <cameolynn@msn.com>
2015-09-30 14:44:52 -0500

    Added diplodocus dinosaur

---
+++ src/com/cameo/Dinosaur.java
@@ -0,0 +1,11 @@
+package com.cameo;
+
+/**
+ * Created by Cameo on 9/30/2015.
+ */
+public class Dinosaur {
+
+    public void roar(){
+        System.out.println("GRRR!!!!!");
+    }
+}
---
+++ src/com/cameo/DinosaurSupervisor.java
@@ -0,0 +1,15 @@
+package com.cameo;
+
+public class DinosaurSupervisor {
+
+    public static void main(String[] args) {
+
+    Dinosaur trex = new Dinosaur();
+    Dinosaur velociraptor = new Dinosaur();
+    Dinosaur diplodocus = new Dinosaur();
+
+    trex.roar();
+    velociraptor.roar();
+    diplodocus.roar();
+    }
+}
